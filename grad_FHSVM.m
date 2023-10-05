f5547c1559170c499debf02f7c8af9224377e0fc
Fussiji <50704038+Fussiji@users.noreply.github.com>
2023-10-05 12:39:33 +0800

    grad_FHSVM.m

--- grad_FHSVM.m
+++ grad_FHSVM.m
@@ -1,7 +1,6 @@
-% 某一个样本的梯度
 function[f]=grad_FHSVM(X,Y,i,alpha,b,C,type,gamma,K,l)
 k=kernel(X,X(i,:),type,gamma,1);
-e=1-Y(i,:)*k'*alpha;%e可能大于0，可能小于0
+e=1-Y(i,:)*k'*alpha;
 sgn=e;sgn(sgn>0)=1;sgn(sgn<=0)=0;
 f=1/l*K*alpha-sgn*C*b*k*Y(i,:)/((1+b*e)^2);
 end
