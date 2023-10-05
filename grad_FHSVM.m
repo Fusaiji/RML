e757cfc3c54543606368028800d25d6dc2e23702
Fussiji <50704038+Fussiji@users.noreply.github.com>
2023-10-05 12:35:30 +0800

    grad_FHSVM.m

--- grad_FHSVM.m
+++ grad_FHSVM.m
@@ -1,7 +1,7 @@
-% Ä³Ò»¸öÑù±¾µÄÌÝ¶È
-function[f]=grad(X,Y,i,alpha,b,C,type,gamma,K,l)
+% æŸä¸€ä¸ªæ ·æœ¬çš„æ¢¯åº¦
+function[f]=grad_FHSVM(X,Y,i,alpha,b,C,type,gamma,K,l)
 k=kernel(X,X(i,:),type,gamma,1);
-e=1-Y(i,:)*k'*alpha;%e¿ÉÄÜ´óÓÚ0£¬¿ÉÄÜÐ¡ÓÚ0
+e=1-Y(i,:)*k'*alpha;%eå¯èƒ½å¤§äºŽ0ï¼Œå¯èƒ½å°äºŽ0
 sgn=e;sgn(sgn>0)=1;sgn(sgn<=0)=0;
 f=1/l*K*alpha-sgn*C*b*k*Y(i,:)/((1+b*e)^2);
 end
